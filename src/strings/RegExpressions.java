00f1df98266e855216cc27255598d3a0b6af2e3a
ikrasnova <i.bas@me.com>
2021-12-13 20:44:31 +0200

    HW20

--- src/strings/RegExpressions.java
+++ src/strings/RegExpressions.java
@@ -23,25 +23,38 @@ public static String ipV4Part() {
  * opernds integer positive number with possible leading zero's
  */
 public static String arithmeticExpression() {
-	String delimiter = "\\s*";// любой бeлый символ может бфтьможет не быть
-	String operand = "\\d+";// хотябы одна цифра
+	String delimiter = "\\s*";
+	String operand = "\\d+";
 	String operator = "[+*/-]";
 	return String.format("%1$s%2$s(%1$s%3$s%1$s%2$s)*%1$s", delimiter,operand,operator);
 //	return delimiter + operand + "(" +delimiter + operator + delimiter + operand+")"+"*" + delimiter;
 //	return "\\s*\\d+(\\s*[+*/-]\\s*\\d+)*\\s*";
 }
-
+/**
+ * 
+ * @return regex for IpV4
+ * contains four parts separated by dot
+ * each part is the  regex of String ipV4Part()
+ */
+//TODO done
 public static String ipV4() {
-	String ipNum = "(\\d\\d?|[0-1]\\d{2}|2[0-4]\\d|25[0-5])";
+	String ipNum = "("+ ipV4Part()+")";
 	String dilimetr = "[.]";
-	return String.format("(%1$s%2$s){3}%1$s" , ipNum,dilimetr);
-	//return "(" +ipNum + dilimetr +")"+"{3}" + ipNum;
+	//return String.format("(%1$s%2$s){3}%1$s" ,ipNum, dilimetr);
+	return "(" + ipNum + dilimetr +")"+"{3}" + ipNum;
 	
-}
-public static String telephoneNumberIsrael() {
-	String prefiks = "(\\+972\\-?|0)";
-	String code = "5\\d";
-	String number ="(\\-?\\d){6}\\d";
+}/**
+ * 
+ * @return regex for mobile Israel phone
+ * Israel code optional +972 (if the code specified, operator code should be with no 0)
+ * operator code: 050-059 , 072-077
+ * 7 digits that may or may not be separated by dash
+ */
+//TODO done
+public static String mobileIsraelPhone() {
+	String prefiks = "(\\+972\\-?\\s*|0)";
+	String code = "(5\\d|7[2-7])";
+	String number ="(\\-?\\d){7}";
 	//return String.format("%1$s%2$s%3$s", prefiks, code, number);
 	return prefiks+code+ number;
 }
--- src/strings/RegExpressionsTest.java
+++ src/strings/RegExpressionsTest.java
@@ -94,68 +94,99 @@ class RegExpressionsTest {
 		assertFalse("  20+&".matches(arithmeticExpression()));
 		assertFalse(" +20".matches(arithmeticExpression()));
 	}
-	@Test
-	void ipV4TrueTest() {
-		assertTrue("0.0.0.0".matches(ipV4()));
-		assertTrue("022.0.04.055".matches(ipV4()));
-		assertTrue("255.0.000.0".matches(ipV4()));
-		assertTrue("05.033.02.011".matches(ipV4()));
-		
-
-	}
-	@Test
-	void ipV4TrueFalse() {
-		assertFalse("0000.88.9".matches(ipV4()));
-		assertFalse("00 88.9.233".matches(ipV4()));
-		assertFalse("0.88.9.256".matches(ipV4()));
-		assertFalse("00.88-9".matches(ipV4()));
-		assertFalse("0000.88.9.55.33".matches(ipV4()));
-
-	}
-	@Test
-	void telNumIstTrueTest() {
-		assertTrue("+972-534123456".matches(telephoneNumberIsrael()));
-		assertTrue("+972534123456".matches(telephoneNumberIsrael()));
-		assertTrue("+97253-4123456".matches(telephoneNumberIsrael()));
-		assertTrue("+9725341-23-456".matches(telephoneNumberIsrael()));
-		assertTrue("0534123456".matches(telephoneNumberIsrael()));
-		assertTrue("054-712-34-56".matches(telephoneNumberIsrael()));
-		
-	}
-	@Test
-	void telNumIstFalseTest() {
-		assertFalse("972534123456".matches(telephoneNumberIsrael()));//no +
-		assertFalse("+9720534123456".matches(telephoneNumberIsrael()));//0 after +972
-		assertFalse("053-412_456".matches(telephoneNumberIsrael()));//_ disallowed
-		assertFalse("534123456".matches(telephoneNumberIsrael()));//wrong prefix
-		assertFalse("+97253412345".matches(telephoneNumberIsrael()));//too few digits
-		assertFalse("05341234567".matches(telephoneNumberIsrael()));
-	}
-	@Test
-	void StringSplitTest() {
-		String str = "Regular_Expressions_are_hard";
-		String regEx ="_";
-		String[] exp = {"Regular", "Expressions", "are", "hard"};
-		assertArrayEquals(exp, str.split(regEx));
-		boolean fl = false;
-//		try {
-//			exp = str.split("888");
-//			
-//		} catch (PatternSyntaxException e) {
-//			fl = true;
-//			System.out.println("Catching PatternSyntaxException ");
-//		}
-//		assertTrue(fl);
-		String[] exp2 = {"R","gular_Expr","ssions_ar","_hard"};
-		assertArrayEquals(exp2, str.split("e"));
-}
 
 	@Test
-	void StringReplaseAllTest() {
-		String str = "new e-mail adress: Jmai2lll@e-mail.com";
-		String regEx = "(\\w){8}@e\\-mail.(\\w){3}";
-		String exchange = "i.bas@me.com";
-		assertEquals("new e-mail adress: i.bas@me.com", str.replaceAll(regEx, exchange));
+ 	void ipV4PartsTrueTest() {
+ 		assertTrue("000.000.000.000".matches(ipV4()));
+ 		assertTrue("1.2.3.4".matches(ipV4()));
+ 		assertTrue("255.255.255.255".matches(ipV4()));
+ 		assertTrue("190.20.03.004".matches(ipV4()));
+
+ 	}
+ 	@Test
+ 	void ipV4PartsFalseTest() {
+ 		assertFalse("000.000.000.000.".matches(ipV4()));
+ 		assertFalse("000.000.".matches(ipV4()));
+ 		assertFalse("...".matches(ipV4()));
+ 		assertFalse(".100.200.".matches(ipV4()));
+ 		assertFalse("255,255,255,255".matches(ipV4()));
+ 	}
+ 	@Test
+ 	void mobileIsraelTrueTest() {
+ 		//Israel code optional +972
+ 		//code operator 050, 051, 052, 053, 054, 055,056,057,058, 059
+ 		//code operator 072 - 077
+ 		//7 digits that may or may not be separated by dash
+ 		assertTrue("+972-541234567".matches(mobileIsraelPhone()));
+ 		assertTrue("0541234567".matches(mobileIsraelPhone()));
+ 		assertTrue("074-1-2345-67".matches(mobileIsraelPhone()));
+ 		assertTrue("+972541234567".matches(mobileIsraelPhone()));
+ 		assertTrue("+972  541234567".matches(mobileIsraelPhone()));
+ 	}
+ 	@Test
+ 	void mobileIsraelFalseTest() {
+ 		//Israel code optional +972
+ 		//code operator 050, 051, 052, 053, 054, 055,056,057,058, 059
+ 		//code operator 072 - 077
+ 		//7 digits that may or may not be separated by dash
+ 		assertFalse("+972-0541234567".matches(mobileIsraelPhone()));
+ 		assertFalse("+9720541234567".matches(mobileIsraelPhone()));
+ 		assertFalse("972-541234567".matches(mobileIsraelPhone()));
+ 		assertFalse("0641234567".matches(mobileIsraelPhone()));
+ 		assertFalse("+972-54123v567".matches(mobileIsraelPhone()));
+ 	}
+ 	@Test 
+ 	void replaceAllTest() {
+ 		String expr = " 20 +10 * 2	/100 +4     ";
+ 		String actual = getStringWithoutSpaces(expr);
+ 		String expected = "20+10*2/100+4";
+ 		assertEquals(expected, actual);
+
+ 	}
+
+ 	private String getStringWithoutSpaces(String str) {
+ 		// TODO write this method based on the method replaceAll of the class String
+ 		//done
+ 		String regExpSpases ="\\s";
+ 		String strWithoutSpaces = "";
+ 		return str.replaceAll(regExpSpases, strWithoutSpaces);
+ 	}
+ 	@Test
+ 	void splitTest () {
+ 		String expr = " 20 +10 * 2	/100 +4     ";
+ 		String [] operatorsExp = {"", "+", "*", "/", "+"};
+ 		assertArrayEquals(operatorsExp, getOperatorsExpression(expr));
+ 		String [] operandsExp = {"20", "10", "2", "100", "4"};
+ 		assertArrayEquals(operandsExp, getOperandsExpression(expr));
+ 		assertArrayEquals(operandsExp, getOperandsExpression2(expr));
+
+ 	}
+
+ 	private String[] getOperandsExpression(String expr) {
+ 		// TODO the method returns array of strings containing only the operands of the given expression
+ 		// see test. Based on the method split of the class String
+ 		//Done
+ 		String delimiter = "(\\s*)";// 
+ 		String operator = "[+*/-]";
+ 		String str = expr.trim();
+ 		return str.split(delimiter+operator+delimiter);
+ 		//return str.split("\\s*[+*/-]\\s*");
+ 	}
+ 	private String[] getOperandsExpression2(String expr) {
+ 		// TODO second version
+ 		//Done
+ 		String operator = "[+*/-]";
+ 		String str = getStringWithoutSpaces(expr);
+ 		return str.split(operator);
+ 	}
+ 	private String[] getOperatorsExpression(String expr) {
+ 		// TODO the method returns array of strings containing the operators of the given expression
+ 		// with empty string as the first string (see test)
+ 		//based on the method split of the class String
+ 		//Done
+ 		return expr.split("\\s*\\d*[^+*/-]");
+ 		
+ 	}
+
 	
-	}
 }
